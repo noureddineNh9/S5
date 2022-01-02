0408625eb492df8bbdec61f2db1e18dfb3572477
noureddine haddad <noureddinehaddad17@gmail.com>
2022-01-02 12:05:41 +0100

    DB Notes

---
+++ BD/Examen_19-20.sql
@@ -0,0 +1,90 @@
+select v.num_serie, v.modele from VOITURE JOIN VENDRE ON( VOITURE.num_serie=VENDRE.num_serie ) where VENDRE.date_v > To_DATE('1-SEP-2019', 'DD-MON-YY');
+
+select ID_M, count(ID_M) from EXPOSER GROUP BY ID_M;
+
+select a.nom, a.prenom from 
+VENDEUR a JOIN (select id_vendeur, count(date_v) num from VENDRE where to_char(date_v, 'YYYY')=2019 group by id_vendeur) b
+ON(a.id_vendeur=b.id_vendeur)
+order by num;
+
+/* Exercice 2 */
+1-
+Alg:
+	R11 = Projection(VENTE, num_vendeur)
+	R12 = Projection(VENDEUR, num_vendeur)
+	R13 = DIFFERENCE(R12, R11)
+	R14 = Jointure(VENDEUR,R13 / VENDEUR.num_serie = R13.num_serie )
+	R15 = Projection(R14, nom, prénom)
+SQL:
+	select nom, prenom 
+	from vendeur 
+	where num_vendeur NOT IN (select NVL(num_vendeur, 0) from VENTE);
+2-
+Alg:
+	// R21 = Produit(VENDEUR, VENDEUR / num_magazin = num_magazin)
+
+SQL:
+	select V1.nom as membre_1, V2.nom as membre_2 from VENDEUR V1, VENDEUR V2 where V1.num_magazin = V2.num_magazin;
+3-
+	select P1.num_piece as piece_1, P2.num_piece as piece_2, P3.num_piece as piece_3, sum(P1.prix_unitaire + P2.prix_unitaire + P3.prix_unitaire) as prix
+	from PIECE P1, PIECE P2, PIECE P3 
+	where prix < 60000;
+
+5-
+	R51 = Jointure(MAGAZIN, VENTE / num_magazin)
+	R52 = Jointure(R51, PIECE / num_piece)
+	R53 = ROUGROUPER_ET_CALCULER(R52, num_magazin, chiffre_affaire : somme(prix_unitaire * quantite))
+
+	select MAGAZIN.num_magazin mag, MAGAZIN.ville ville, sum(PIECE.prix_unitaire * VENTE.quantite) as "chiffre_affaire" from MAGAZIN, VENTE, PIECE
+	where MAGAZIN.num_magazin = VENTE.num_magazin AND VENTE.num_piece = PIECE.num_piece
+	GROUP BY mag, ville;
+
+6-
+	UPDATE PIECE 
+	SET prix_unitaire = prix_unitaire * 2;
+
+8-
+	DELETE from VENDEUR where num_vendeur=23
+
+
+
+****************************** Exam 18/19 **********************************
+
+/* EX 2 */
+1.
+	R11 = Projection(Photographe)
+
+	select * from Photographe;
+2.
+	R21 = Selection(PriseDeVue / dateVue > '30-04-2004' )
+	R22 = Selection(Photographe / prenom=Frederic AND nom=Cresseux)
+	R23 = Jointure(R21, R22 / numPhotog=proprietaire)
+	R24 = Projection(R23 / nom, prenom, dateVue)
+
+	select Ph.nom, Ph.prenom, Pr.dateVue 
+	from Photographe Ph JOIN PriseDeVue Pr 
+	ON (PhnumPhotog=Prproprietaire) 
+	where Pr.dateVue > '30-04-2004' AND Ph.prenom='Frederic' AND Ph.nom='Cresseux';
+3.
+	R31 = Jointure(Photographe, PriseDeVue / numPhotog=proprietaire)
+	R32 = Jointure(R31, Modele / numModele)
+	R33 = Selection(R32 / prenom='Laetitia' AND nom='Casta')
+	R24 = Projection(R23 / numPhotog, nom)
+
+	select Ph.numPhotog, Ph.nom
+	from Photographe Ph JOIN PriseDeVue Pr ON (Ph.PhnumPhotog=Pr.Prproprietaire) 
+	JOIN Module M ON (Pr.numMod=M.numMod) 
+	where Ph.prenom='Laetitia' AND Ph.nom='Casta';
+4.
+	R41 = Projection( Photographe / numPhotog)
+	R42 = Projection( Commande / numPhotog)
+	R43 = Intersection(R41, R42)
+	R44 = Jointure(Photographe, R43 / numPhotog)
+	R45 = Selection(R44 / nom, prenom)
+
+	select nom, prenom 
+	from Photographe 
+	where numPhotog NOT IN (select numPhotog from Commande);
+
+5.
+	il calcule le nombre des prise de vue qu'est fait par le Photographe du numero 11.
--- BD/TP_SQL.sql
+++ BD/TP_SQL.sql
@@ -36,7 +36,7 @@ select last_name as 'Employee', salary as 'Monthly Salary' from employees where 
 
 /* Q7 */
 select last_name, hire_date from employees where substr(hire_date, 7, 8) = 94;
-select last_name, hire_date from employees where hire_date like '__/__/94';
+select last_name, hire_date from employees where hire_date like '%94';
 
 /* Q8 */
 select last_name, job_id from employees where manager_id is null;
@@ -64,34 +64,128 @@ select last_name as 'Employee', salary as 'Monthly Salary', commission_pct as 'C
 
 
 /* ------------- Exercice 3 -------------- */
+/*Q1*/
+SELECT sysdate "Date"
+FROM dual;
+
+/*Q2 et Q3*/
+SELECT employee_id, last_name, salary, ROUND(salary * 1.155, 0) "New Salary" 
+FROM employees;
+
+/*Q4*/
+SELECT employee_id, last_name, salary, ROUND(salary * 1.155, 0) "New Salary" ROUND(salary * 1.155, 0) - salary "Increase" 
+FROM employees;
+
+/*Q5*/
+SELECT INITCAP(last_name) "Name", LENGTH(last_name) "Length" FROM employees WHERE SUBSTR(last_name, 0, 1) IN('J','M','A') ORDER BY last_name;
+
+/*Q6*/
+SELECT INITCAP(last_name) "Name", LENGTH(last_name) "Length" FROM employees WHERE SUBSTR(last_name, 0, 1)='&start_letter' ORDER BY last_name;
+
+/*Q7*/
+SELECT INITCAP(last_name) "Name", LENGTH(last_name) "Length" FROM employees WHERE UPPER(SUBSTR(last_name, 0, 1))=UPPER('&start_letter') ORDER BY last_name;
 
 
-/* Q1 */
-select SYSDATE as "Date" from dual ;
+/*Q8*/
+SELECT last_name, ROUND(MONTHS_BETWEEN(SYSDATE, hire_date)) "MONTHS_WORKED" FROM employees ORDER BY 2;
 
-select employee_id, last_name, salary, trunc(salary*1.155,0)as "New Salary" from employees;
+/*Q9*/
+SELECT last_name, LPAD(salary, 15, '$') SALARY FROM employees;
 
-select employee_id, last_name, salary, trunc(salary*1.155,0)as "New Salary", trunc(salary*0.155,0) as "Increase" from employees;
+/*Q10*/
+SELECT RPAD(last_name, 8) ||' '|| RPAD(' ', salary/1000+1, '*') EMPLOYEES_AND_THEIR_SALARIES FROM employees ORDER BY salary DESC;
 
-select Initcap(last_name)as "Name",length(last_name)as "Length" from employees where substr(last_name,0,1) in ('A','J','M') order by last_name ;                                                                                                                         
-
-select Initcap(last_name)as "Name",length(last_name)as "Length" from employees where substr(last_name,0,1)='&cara' order by last_name;
-
-select Initcap(last_name)as "Name",length(last_name)as "Length" from employees where substr(last_name,0,1)=upper('&cara') order by last_name;
-
-select DISTINCT last_name , round(months_between(sysdate,hire_date),0) as "Months_Worked" from employees order by "Months_Worked" ASC;
-
-select last_name, lpad(salary,15,'$') from employees; 
-
-select substr(last_name,1,8) || ' ' || LPAD(' ', salary / 1000,'*')as "EMPLOYEES AND THEIR SALARIES" from employees;
-
-/* Q11 */
-select last_name, round((sysdate-hire_date)/7, 0) TENURE from employees where department_id=90 order by TENURE desc;
+/*Q11*/
+select last_name, TRUNC((sysdate-hire_date)/7) TENURE from employees where department_id=90 ORDER BY TENURE;
 
 
 /* ------------- Exercice 4 -------------- */
-/* Q2 */
-select last_name, hire_date, TO_CHAR(hire_date, 'DAY, "the" ddspth "of" mounth, YYYY') REVIEW from employees;  
+/*Q1*/
+SELECT ( LAST_NAME || ' earns ' || TO_CHAR(SALARY,'$99,999.99') || ' monthly but wants ' ||  TO_CHAR(3*SALARY,'$99,999.99') ) as "dream salary" 
+FROM EMPLOYEES;
 
-/* Q3 */
-select last_name, hire_date, TO_CHAR(hire_date, 'DAY') DAY from employees order by mod(TO_CHAR(hire_date, 'D') + 5, 7);
+/*Q2*/
+SELECT LAST_NAME , HIRE_DATE , TO_CHAR(NEXT_DAY(ADD_MONTHS(HIRE_DATE,6),'LUNDI'),'DAY,"the" DDspth "OF" Month YYYY')  as  REVIEW
+FROM EMPLOYEES;
+
+/*Q3*/
+SELECT LAST_NAME ,TO_CHAR(HIRE_DATE,'DD MON YY'),TO_CHAR(HIRE_DATE,'DAY') AS DAY, TO_CHAR(HIRE_DATE,'D')
+FROM EMPLOYEES
+ORDER BY TO_CHAR(HIRE_DATE,'D');
+
+/*Q4*/
+SELECT LAST_NAME , NVL(TO_CHAR(SALARY *COMMISSION_PCT) , 'NO COMM') 
+FROM EMPLOYEES;
+
+/*Q5*/
+SELECT JOB_ID  , DECODE( JOB_ID  ,'AD_PRES' , 'A'
+                              ,'ST_MAN'  , 'B'
+                              ,'IT_PROG' , 'C'
+                              ,'SA_REP'  , 'D'
+                              ,'ST_CLERK' , 'E'
+                              ,0
+                       ) GRADE
+FROM EMPLOYEES;
+
+/*Q6*/
+SELECT JOB_ID ,  CASE JOB_ID  WHEN 'AD_PRES' THEN 'A'
+                              WHEN 'ST_MAN' THEN  'B'
+                              WHEN'IT_PROG' THEN 'C'
+                              WHEN'SA_REP'  THEN 'D'
+                              WHEN'ST_CLERK' THEN 'E'
+                              ELSE '0'                       
+                END GRADE
+FROM EMPLOYEES;
+
+/* ------------- Exercice 5 -------------- */
+
+/*Q1*/
+VRAI
+
+/*Q2*/
+FAUX
+
+/*Q3*/
+VRAI
+
+/*Q4*/
+SELECT max(salary) Maximum, min(salary) Minimum, sum(salary) Sum, avg(salary) Average FROM employees
+
+/*Q5*/
+SELECT job_id, max(salary) Maximum, min(salary) Minimum, sum(salary) Sum, avg(salary) Average FROM employees GROUP BY job_id;
+SELECT job_id, count(*) FROM employees GROUP BY job_id;
+
+/*Q6*/
+SELECT job_id, count(*) FROM employees where job_id='&job' GROUP BY job_id;
+
+/*Q7*/
+SELECT count(DISTINCT manager_id) FROM employees where job_id=null;
+
+/*Q8*/
+SELECT max(salary)-min(salary) DIFFERENCE FROM employees;
+
+/*Q9*/
+SELECT manager_id, min(salary) 
+FROM employees
+WHERE manager_id IS NOT NULL
+GROUP BY manager_id
+HAVING min(salary)>6000
+ORDER BY min(salary) DESC;
+
+/*Q10*/
+SELECT COUNT(*) total,
+       SUM(DECODE(TO_CHAR(hire_date,'YYYY'), 1995, 1, 0)) "1995", 
+       SUM(DECODE(TO_CHAR(hire_date,'YYYY'), 1996, 1, 0)) "1996", 
+       SUM(DECODE(TO_CHAR(hire_date,'YYYY'), 1997, 1, 0)) "1997", 
+       SUM(DECODE(TO_CHAR(hire_date,'YYYY'), 1998, 1, 0)) "1998"
+FROM employees;
+
+/*Q11*/
+SELECT job_id "Job",
+SUM(DECODE(department_id, 20, salary)) "Dept 20",
+SUM(DECODE(department_id, 50, salary)) "Dept 50",
+SUM(DECODE(department_id, 80, salary)) "Dept 80",
+SUM(DECODE(department_id, 90, salary)) "Dept 90",
+SUM(salary) "Total"
+FROM employees
+GROUP BY job_id;
